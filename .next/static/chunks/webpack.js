9127d2531f11611653fcc4c148f8d0038dace4dd
FareaaFaisal <faisalaftab326@gmail.com>
2024-10-16 08:44:55 +0500

    Added new files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fa6f111371dca3b9"; }
+/******/ 		__webpack_require__.h = function() { return "810c8af089d37b6d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.9181a470904b0754.hot-update.js
+++ .next/static/webpack/webpack.9181a470904b0754.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "fa6f111371dca3b9"; }
+/******/ 	__webpack_require__.h = function() { return "fa11703d975c251b"; }
 /******/ }();
 /******/ 
 /******/ }
